f4dbd74e0dd2f66ccbc7a4a3d313e8fb251274cf
KaleidoVR <KaleidoVR@hotmail.com>
2026-09-20 22:15:08 -0700

    Add Masks to Settings (Beta).

--- Unity Organizer Tool/Editor/KaleidoAssetOrganizer.cs
+++ Unity Organizer Tool/Editor/KaleidoAssetOrganizer.cs
@@ -4985,6 +4985,7 @@ namespace KaleidoVR.EditorTools
             layout.roughness = DrawChildFolderField("Roughness", layout.roughness, layout.textures);
             layout.ao = DrawChildFolderField("AO", layout.ao, layout.textures);
             layout.icons = DrawChildFolderField("Icons", layout.icons, layout.textures);
+            layout.textureMasks = DrawChildFolderField("Masks", layout.textureMasks, layout.textures);
             GUILayout.Space(6);
             layout.audio = DrawParentFolderField("Audio", layout.audio);
             layout.prefabs = DrawParentFolderField("Prefabs", layout.prefabs);
